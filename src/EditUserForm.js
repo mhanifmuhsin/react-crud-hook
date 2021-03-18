2994a953f7f53ee92c2be3b66d66fd793c393c55
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2021-03-18 11:37:55 +0700

    update function

--- src/EditUserForm.js
+++ src/EditUserForm.js
@@ -8,11 +8,6 @@ const EditUserForm = (props) => {
 
     const [user, setUser] = useState(props.currentUser);
 
-    const handleChange = e => {
-        const { name, value } = e.target;
-        setUser({ ...user, [name]: value });
-    }
-
     const handleSubmit = e => {
         e.preventDefault();
         if (user.name && user.username && user.email && user.division) props.updateUser(user);
@@ -20,16 +15,16 @@ const EditUserForm = (props) => {
     return (
         <div className="container">
             <form>
-                <div className="row">
+            <div className="row">
                     <div className="six columns">
                         <label>Name</label>
-                        <input className="u-full-width" type="text" name="name" value={user.name} onChange={handleChange} />
+                        <input className="u-full-width" type="text" name="name" value={user.name} onChange={e => setUser({ ...user, name: e.target.value })} />
                         <label>Username</label>
-                        <input className="u-full-width" type="text" name="username" value={user.username} onChange={handleChange} />
+                        <input className="u-full-width" type="text" name="username" value={user.username} onChange={e => setUser({ ...user, username: e.target.value })} />
                         <label>Email</label>
-                        <input className="u-full-width" type="email" name="email" value={user.email} onChange={handleChange} />
-                        <label for="division">Division</label>
-                        <select class="u-full-width" id="division" name="division" value={user.division} onChange={handleChange}>
+                        <input className="u-full-width" type="email" name="email" value={user.email} onChange={e => setUser({ ...user, email: e.target.value })} />
+                        <label htmlFor="division">Division</label>
+                        <select className="u-full-width" id="division" name="division" value={user.division} onChange={e => setUser({ ...user, division: e.target.value })}>
                             <option value="">Select</option>
                             <option value="Database Administrator">Database Administrator</option>
                             <option value="Back End Developer">Back End Developer</option>
@@ -39,14 +34,14 @@ const EditUserForm = (props) => {
                     <div className="six columns">
                         <label>Gender</label>
                         <label>
-                            <input className="u-cf" type="radio" name="gender" value={user.email} onChange={handleChange} />Male
-                            <input className="u-cf" type="radio" name="gender" value={user.email} onChange={handleChange} />Female
-                        </label>
-                        <label for="message">Message</label>
-                        <textarea class="u-full-width" placeholder="Hi Dave …" id="message"></textarea>
-                        <label class="send-yourself-copy">
-                            <input type="checkbox" />
-                            <span class="label-body">Send a copy to yourself</span>
+                            <input className="u-cf" type="radio" name="gender" checked={user.gender === 0} onChange={e => setUser({ ...user, gender: 0})} />Male
+                            <input className="u-cf" type="radio" name="gender" checked={user.gender === 1} onChange={e => setUser({ ...user, gender: 1 })} />Female
+            </label>
+                        <label htmlFor="message">Message</label>
+                        <textarea className="u-full-width" placeholder="Hi Dave …" id="message" name="message" value={user.message} onChange={e => setUser({ ...user, message: e.target.value })}></textarea>
+                        <label className="send-yourself-copy">
+                            <input type="checkbox" checked={user.sendcopy} onChange={e => setUser({ ...user, sendcopy: e.target.checked })} />
+                            <span className="label-body">Send a copy to yourself</span>
                         </label>
                         <button className="button-primary" type="submit" onClick={handleSubmit} >Edit user</button>
                         <button type="submit" onClick={() => props.setEditing(false)} >Cancel</button>
